74fab28ef202a1541c86edc3680b5b6cd7b35578
Alex Schneider <aschne19@lion.lmu.edu>
2014-10-08 10:32:05 -0700

    Update test and index.html

--- test/rpg_test.dart
+++ test/rpg_test.dart
@@ -1,11 +1,10 @@
 import 'package:unittest/unittest.dart';
 import 'package:unittest/html_enhanced_config.dart';
 
-import 'package:http/browser_client.dart';
+
 
 void main() {
-  HttpRequest.request('https://lmu-diabolical.appspot.com/characters', method: 'GET', responseType: 'json')
-  .then((HttpRequest res) {
-    print(res.response);
+  group("Adding characters", () {
+    
   });
 }
